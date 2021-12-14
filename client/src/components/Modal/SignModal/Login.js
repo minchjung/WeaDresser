f40f5f4ff053dfa2c97f6b4f7f0b7684c0756dd1
minch jung <minch@minchui-MacBookAir.local>
2021-12-14 16:08:25 +0900

    Refactor: Modal, Sifnup, SigmForm, useForm, useSlider ok

--- client/src/components/Modal/SignModal/Login.js
+++ client/src/components/Modal/SignModal/Login.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import { useSpring } from 'react-spring'
-import { useForm } from "../../../utils/useForm";import Signup from "./Signup";
 import title from './title.svg'
 import { CloseModalButton } from "./ModalStyle";
 import { useLoginApi} from '../../../utils/api/useLoginApi'
-import { isShowSignUpModalHandler} from '../../../redux/actions/actions'
 import { LoginContainer,LogoContainer,InputContainer,LoginError,LoginBtnContainer } from './LoginStyle';
+// import { isShowLoginModalHandler, isShowSignUpModalHandler} from '../../../redux/actions/actions'
 require('dotenv').config();
 
+<<<<<<< HEAD
 function Login({ modalChangeHandler }){
   const [ loginInfo, setLoginInfo ] = useState({ email: "", password: "" });
   // const [ errorMessage, setErrorMessage ] = useState("");
@@ -26,6 +24,12 @@ function Login({ modalChangeHandler }){
     transform: isShowSignUpModal ? 'translateX(0%)' : 'translateX(100%)', 
     opacity : isShowSignUpModal ? 1 : 0 
   })
+=======
+function Login({ closeModalByBtn, displayLogin, modalChangeHandler }){
+  const [ loginInfo, setLoginInfo ] = useState({ email: "", password: "" });
+  const { getGoogleAccToken, getKakaoCode, validCheckHandler, 
+    errorMessage, setErrorMessage, active, setActive,  } = useLoginApi();
+>>>>>>> f3dcdc5 (Refactor: Modal, Sifnup, SigmForm, useForm, useSlider)
 
   // inputvalue save to the loginInfo States
   const handleInputValue = (key) => (e) => {
@@ -36,46 +40,8 @@ function Login({ modalChangeHandler }){
   const handleKeyPress = (e) => {
     if(e.key === 'Backspace') setActive("")
   };
-  // validition : email=null, password=Null, email regx@ 
-  const validCheckHandler = () => {
-    const { email, password } = loginInfo
-
-    setActive("")
-    if(!email){
-      setErrorMessage('이메일를 입력해 주세요')
-    }
-    else if( !pattern.test(email) || !email.includes('@') || !email.includes('.') ){
-      setErrorMessage("이메일 형식을 확인해 주세요")
-    }
-    else if(!password){
-      setErrorMessage('비밀번호를 입력해 주세요')
-    }
-    //! 6자리 이상 으로 교체
-    else if(password.length < 3){
-      setErrorMessage('3자리 이상 비밀번호를 입력해 주세요')
-    }
-    else{
-      setActive("-active") // button active
-      userLoginHandler() // * login ajax call
-    }
-        
-  };
-
-  // * 일반 유저 로그인 done ! 
-  const userLoginHandler = async () => {
-    const{ email, password } = loginInfo;
-    handleUserLoginApi({ email, password})
-    //! server uri dotenv 안될때 있음!
-  }
-  const modalChangeHandler = () => {
-    dispatch(isShowSignUpModalHandler(true));
-  }
   return (
-    <>{ isShowSignUpModal ? 
-      <LoginContainer style={displaySignup}>
-        <Signup closeModalByBtn={closeModalByBtn}/> 
-      </LoginContainer>
-      :
+    <>
       <LoginContainer style={displayLogin}>
         <LogoContainer><img alt="Weadresser" src={title}/></LogoContainer>
         <InputContainer>
@@ -96,14 +62,13 @@ function Login({ modalChangeHandler }){
             <LoginError>{errorMessage}</LoginError>
         </InputContainer>
         <LoginBtnContainer>
-          <button onClick={validCheckHandler}  className={`login-btn${active}`}> 로그인</button>
+          <button onClick={()=>{ validCheckHandler(loginInfo)}}  className={`login-btn${active}`}> 로그인</button>
           <button onClick={modalChangeHandler} className='singup-btn'>회원가입</button>
           <button onClick={getKakaoCode} className='kakao-btn'>Kakao</button>
           <button onClick={getGoogleAccToken} className='google-btn'>Google</button>
         </LoginBtnContainer>
         <CloseModalButton onClick={closeModalByBtn}/>
       </LoginContainer>
-      }
     </>
   );
 }
