5c6b340fc531466e7012b19b17e6fcc1986bef23
Minchan <78589751+minchjung@users.noreply.github.com>
2022-03-19 21:21:22 +0900

    Fixed : err handle (and git flow err)

--- server/controllers/tokenfunction/index.js
+++ server/controllers/tokenfunction/index.js
@@ -9,9 +9,6 @@ module.exports = {
   },
 
   sendToken: (res, token) => {
-<<<<<<< HEAD
- 
-=======
     res.cookie("Bearer", token, {
       httpOnly: true,
       sameSite: "none",
@@ -22,29 +19,15 @@ module.exports = {
       // overwrite: true,
       // signed : true
     });
-    // res.cookie("Login", "true", {
-    //   httpOnly: true,
-    //   sameSite: "none",
-    //   secure: true,
-    //   maxAge: 60 * 60 * 24 * 1000,
-    //   domain: "localhost",
-    //   path: "/",
-    //   ovewrite: true,
-    //   signed : true
-    // });
->>>>>>> 19e8a8d (before to clear for cl)
   },
 
   isAuthorized: (req) => {
 
-<<<<<<< HEAD
-=======
     const cookieToken = req.cookies.Bearer;
     if (!cookieToken) return null;
     try {
       return verify(cookieToken, process.env.ACCESS_SECRET);
->>>>>>> 62ccd36 ([modify] sidebar 일부 수정, edit요청 수정)
-
+    catch(err){ return false }
   },
 
   isValid: (email, id) => {
